e6ab998f11793ffd1cfc9d8db758c13cb16dd52f
Wesley Batista Alcantara <wesleydevadm@gmail.com>
2024-02-20 21:27:35 -0300

    Ajustes Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,24 +4,27 @@ import Icon from "../Icon/Icon.jsx";
 export default function Footer() {
     return (
     <div className="footer-container">
-        <div className="footer-contact">
-          <h3 className="contact-title">
-            Contatos
-          </h3>
-          <p>
-            <ul className="contact-info">
-              <li>
-                Email: wesleybaadm@gmail.com
-              </li>
-              <li>
-                Cel: +55 31 99454-2710
-              </li>
-            </ul>
-          </p>
-        </div>
-        <div class="social-links">
-            <div class="social-links-container">
-                <h3>Redes Sociais</h3>
+        <div className="footer-container-contact">
+          <div>
+            <h3 className="contact-title">
+              Contatos
+            </h3>
+            <p>
+              <ul className="contact-info">
+                <li>
+                  Email: wesleybaadm@gmail.com
+                </li>
+                <li>
+                  Cel: +55 31 99454-2710
+                </li>
+              </ul>
+            </p>
+          </div>
+          <div class="social-links-container">
+              <h3 className="social-links-title">
+                Redes Sociais
+              </h3>
+              <div className="social-links-info">
                 <a href="https://github.com/wesleyalcantara">
                     <Icon name="github" />
                 </a>
@@ -31,12 +34,13 @@ export default function Footer() {
                 <a href="https://www.instagram.com/wesley.alcantara_">
                     <Icon name="instagram" />
                 </a>
-            </div>
+              </div>
+          </div>
         </div>
         <div class="footer-info">
             <p>
                 <ul>
-                    <li>Desenvolvido por: Wesley Batista Alcantara</li>
+                    <li> Desenvolvido por: Wesley Batista Alcantara</li>
                 </ul>
             </p>
         </div>
